58bda86a0336c80d472cb44ba4dc00e289face1b
Mary-Jane Davies <mjpunkin@outlook.com>
2025-05-11 15:35:47 -0700

    Added run.bat
    Added a bat script to quickly run all the necessary servers locally

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,19 +2,6 @@ use actix_web::{web, get, HttpServer, App, Responder, HttpResponse};
 use actix_web::web::Path;
 use routes::configure_routes;
 
-// One endpoint with get method
-#[get("/home")]
-async fn home() -> impl Responder {
-    let response: &str = "Welcome to Actix Web Server";
-    HttpResponse::Ok().body(response)
-}
-
-// One endpoint with get method
-#[get("/hello/{firstname}/{lastname}")]
-async fn hello_user(params: Path<(String, String)>) -> impl Responder {
-    let response: String = format!("Hello {} {}", params.0, params.1);
-    HttpResponse::Ok().body(response)
-}
 mod routes;
 mod db;
 mod handlers;
@@ -30,8 +17,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .configure(configure_routes) // Attach route configurations
-            .service(home)
-            .service(hello_user)
     })
     .bind(("127.0.0.1", 8000))?
     .run()
